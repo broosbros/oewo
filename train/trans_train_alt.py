7630f5228f6be5fad24e5120933d3c46528daeca
broosbros <veerhasbeenfound@gmail.com>
2024-09-02 15:54:24 +0530

    Add files via upload

--- train/trans_train_alt.py
+++ train/trans_train_alt.py
@@ -144,7 +144,7 @@ def train(model, dataloader, num_epochs, criterion, optimizer, output_dir, log_d
 
         for batch_idx, (lr_img, hr_img, gt_low_freq, gt_high_freq) in tqdm(enumerate(
             dataloader
-        ),desc="Batch", unit="Batches"):
+        ),desc="Batch", unit="Batches",total="len(dataloader)"):
             lr_img = lr_img.to(device)
             hr_img = hr_img.to(device)
             gt_low_freq = gt_low_freq.to(device)
